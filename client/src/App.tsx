cd41470c26c4d1e2054968a8df6578325ec4abfc
Benjamin Forster <benjamin.forster@reaktor.com>
2020-03-27 21:54:43 +0200

    placeholder app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import img1 from './1.jpeg';
+import img2 from './2.jpeg';
+
 
 function App() {
+  const [flip, setFlip ] = useState(true);
+  useEffect(() => {
+    let timer = requestAnimationFrame(loop);
+    let previousTime: number;
+    function loop(t: number) {
+      if (!previousTime) previousTime = t;
+      const diff = t - previousTime
+      if (diff > 200 ) {
+        setFlip(f => !f);
+        previousTime = t;
+      }
+      timer = requestAnimationFrame(loop);
+    }
+    return () => {
+      cancelAnimationFrame(timer);
+    }
+  })
   return (
     <div className="App">
-      <header><h1>l0ve.stream</h1></header>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquam aliquid at dignissimos, distinctio dolor ea enim harum inventore ipsam laboriosam neque nisi odio optio provident quam ratione sint voluptas.</p>
+      <header><h1>❤ l0ve.stream ❤</h1></header>
+      <main>
+        <p>hello my love, this is a place holder</p>
+        <div>
+          <img src={flip ? img1 : img2 }/>
+        </div>
+        <p>it's working though</p>
+        <p>we now have a website</p>
+        <p>which is deployed automatically</p>
+        <p>( ( ( ❤ ) ) )️</p>
+      </main>
     </div>
   );
 }
