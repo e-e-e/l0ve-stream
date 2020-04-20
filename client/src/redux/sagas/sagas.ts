42037af1c431a5b2eeb58a8638076645504a671e
Benjamin Forster <benjamin.forster@reaktor.com>
2020-04-20 20:36:18 +0300

    formatin

--- client/src/redux/sagas/sagas.ts
+++ client/src/redux/sagas/sagas.ts
@@ -1,7 +1,6 @@
 import { all, fork } from "redux-saga/effects";
-import {playlistSagas} from "./playlists";
-import {userSagas} from "./users";
-
+import { playlistSagas } from "./playlists";
+import { userSagas } from "./users";
 
 export const rootSaga = function* root() {
   yield all([fork(playlistSagas), fork(userSagas)]);
