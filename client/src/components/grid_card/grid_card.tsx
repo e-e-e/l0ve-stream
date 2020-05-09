d96e617bbd9111cc4d0d7a4aee0a76842ba974ea
Benjamin Forster <benjamin.forster@reaktor.com>
2020-05-09 22:15:00 +0300

    add hover effect to grid views

--- client/src/components/grid_card/grid_card.tsx
+++ client/src/components/grid_card/grid_card.tsx
@@ -15,47 +15,53 @@ type GridProps = {
 export const GridCard = (props: GridProps) => {
   const { topLeft, bottomLeft, bottomRight, info, onClick } = props;
   return (
-    <div className={classNames(styles.gridContainer, { [styles.clickable]: !!onClick })} onClick={onClick}>
-      <div className={styles.gridTop}>
-        <div className={classNames(styles.gridLeft, styles.item)}>
-          {topLeft}
-        </div>
-        <div className={styles.gridRight}>
-          <div className={styles.aspect}>
-            <div className={classNames(styles.info, styles.fill)}>
-              <svg style={{ width: "100%", height: "100%" }}>
-                <line
-                  x1="0"
-                  y1="100%"
-                  x2="100%"
-                  y2="0"
-                  style={{ stroke: "rgb(0,0,0)", strokeWidth: 1 }}
-                />
-              </svg>
-            </div>
-            <div
-              className={classNames(styles.infoTop, styles.info, styles.item)}
-            >
-              {info?.top}
-            </div>
-            <div
-              className={classNames(
-                styles.infoBottom,
-                styles.info,
-                styles.item
-              )}
-            >
-              {info?.bottom}
+    <div
+      className={classNames(styles.gridContainer, {
+        [styles.clickable]: !!onClick,
+      })}
+    >
+      <div className={styles.gridWrapper} onClick={onClick}>
+        <div className={styles.gridTop}>
+          <div className={classNames(styles.gridLeft, styles.item)}>
+            {topLeft}
+          </div>
+          <div className={styles.gridRight}>
+            <div className={styles.aspect}>
+              <div className={classNames(styles.info, styles.fill)}>
+                <svg style={{ width: "100%", height: "100%" }}>
+                  <line
+                    x1="0"
+                    y1="100%"
+                    x2="100%"
+                    y2="0"
+                    style={{ stroke: "rgb(0,0,0)", strokeWidth: 1 }}
+                  />
+                </svg>
+              </div>
+              <div
+                className={classNames(styles.infoTop, styles.info, styles.item)}
+              >
+                {info?.top}
+              </div>
+              <div
+                className={classNames(
+                  styles.infoBottom,
+                  styles.info,
+                  styles.item
+                )}
+              >
+                {info?.bottom}
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className={styles.gridBottom}>
-        <div className={classNames(styles.gridLeft, styles.item)}>
-          {bottomLeft}
-        </div>
-        <div className={classNames(styles.gridRight)}>
-          <div className={styles.item}>{bottomRight}</div>
+        <div className={styles.gridBottom}>
+          <div className={classNames(styles.gridLeft, styles.item)}>
+            {bottomLeft}
+          </div>
+          <div className={classNames(styles.gridRight)}>
+            <div className={styles.item}>{bottomRight}</div>
+          </div>
         </div>
       </div>
     </div>
